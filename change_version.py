4e8794f1893091fedd11b88cbcd42bbd3dde9241
Michael C. Stroh <stroh@astroh.org>
2022-12-20 11:52:02 -0600

    updated output

--- change_version.py
+++ change_version.py
@@ -3,4 +3,4 @@ sed "s/X\.XX/$1/g" change_colab_python/default.py > change_colab_python/run_me.p
 chmod u+x change_colab_python/run_me.py
 python change_colab_python/run_me.py
 PY_VERSION=`python --version`
-echo "Installed Python version: $PY_VERSION"
+echo "\nRunning: $PY_VERSION"
